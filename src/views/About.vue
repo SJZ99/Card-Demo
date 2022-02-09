cb1da63e66964fa83fa20886e9517027f8f3b0f7
YuYue <9987jdvrb@gmail.com>
2022-02-09 14:04:06 +0800

    push to github

---
+++ src/views/About.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <v-card width=300 id="card">
+      <v-img src="../assets/keyboard.jpg"></v-img>
+
+      <v-card-title>
+        Title
+      </v-card-title>
+
+
+      <v-card-actions>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="isActive = !isActive">
+          <v-icon :class="{ active : isActive }">mdi-cards-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-bookmark</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-share-variant</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isActive: false,
+  })
+}
+</script>
+
+<style>
+#card {
+  margin: 0 auto;
+}
+.active{ 
+  color: rgb(255, 0, 0) !important;
+}
+</style>
